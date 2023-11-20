f6778599ca992b8bbeca9770ecf15d3c904a3ff9
Carmen Li <cmnli95@gmail.com>
2023-11-19 20:36:01 -0500

    carmen li pushing phase 1 array find methods lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const winningGame = record.find(game => game.result === "W");
+    if (winningGame) {
+        return winningGame.year;
+    } else {
+        return undefined;
+    }
+}
